fadf096fb07a904edab440e4d153bb1e1d5e2ffa
Hameed Ibn Samad <hameedibns@gmail.com>
2025-08-01 10:22:24 +0000

    PRAYER TIME will give at location where the user is that location what t

--- src/hooks/use-prayer-time.tsx
+++ src/hooks/use-prayer-time.tsx
@@ -65,9 +65,9 @@ export const PrayerTimeProvider = ({ children }: PrayerTimeProviderProps) => {
 
     const fetchPrayerTimes = async () => {
       try {
-        const date = new Date();
+        const date = format(new Date(), 'dd-MM-yyyy');
         const response = await fetch(
-          `https://api.aladhan.com/v1/timingsByCity?city=Dubai&country=AE&method=2`
+          `https://api.aladhan.com/v1/timings/${date}?latitude=${location.latitude}&longitude=${location.longitude}&method=2`
         );
         
         if (!response.ok) {
