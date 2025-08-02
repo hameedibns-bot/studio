3449593c7dcfc3f84fe3653db8d3913ad1b7d0e6
Hameed Ibn Samad <hameedibns@gmail.com>
2025-08-02 00:25:45 +0000

    File changes

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "prolife-ai-coach-vprqb",
+  "appId": "1:626551473068:web:344dbb9d0b42490e4d3009",
+  "storageBucket": "prolife-ai-coach-vprqb.firebasestorage.app",
+  "apiKey": "AIzaSyDFuyH2mLy51kh7uQqtZhoH2hl4boajN1s",
+  "authDomain": "prolife-ai-coach-vprqb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626551473068"
 };
 
 // Initialize Firebase
